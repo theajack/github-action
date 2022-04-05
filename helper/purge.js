d33923e760ac388a5f49b67a05234e03ee64a4fe
tackchen <tackchen@tencent.com>
2022-04-05 19:41:28 +0800

    feat: test

---
+++ helper/purge.js
@@ -0,0 +1,7 @@
+// const https = require('https');
+
+// https.get('https://cdn.jsdelivr.net/gh/theajack/github-action/', () => {
+    
+// });
+
+console.log('Purge');
--- helper/test/index.js
+++ helper/test/index.js
@@ -2,7 +2,7 @@
  * @Author: tackchen
  * @Date: 2022-04-05 19:12:29
  * @LastEditors: tackchen
- * @LastEditTime: 2022-04-05 19:12:29
+ * @LastEditTime: 2022-04-05 19:40:46
  * @FilePath: /github-action/helper/test/index.js
  * @Description: Coding something
  */
@@ -10,3 +10,5 @@
 const Str = require('../../npm/index');
 
 console.log(Str);
+
+throw new Error('Test Failed');
