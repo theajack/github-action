cc4ea7140e220d0def965678ef5bb6a2e7c425c2
tackchen <tackchen@tencent.com>
2022-04-05 19:50:26 +0800

    feat: 1

--- helper/test/index.js
+++ helper/test/index.js
@@ -2,13 +2,14 @@
  * @Author: tackchen
  * @Date: 2022-04-05 19:12:29
  * @LastEditors: tackchen
- * @LastEditTime: 2022-04-05 19:40:46
+ * @LastEditTime: 2022-04-05 19:50:04
  * @FilePath: /github-action/helper/test/index.js
  * @Description: Coding something
  */
 
 const Str = require('../../npm/index');
 
+console.log('Test Passed');
 console.log(Str);
 
-throw new Error('Test Failed');
+// throw new Error('Test Failed');
