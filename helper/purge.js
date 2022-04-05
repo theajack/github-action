a13f652ce039132dbc474d8ec076e3d7624ee1a2
theajack <1506282385@qq.com>
2022-04-05 12:46:31 +0000

    Deploying to gh-pages from @ theajack/github-action@813a393265de9c630c89a6c9c3dbb50e99af06c8 🚀

--- helper/purge.js
+++
@@ -1,7 +0,0 @@
-// const https = require('https');
-
-// https.get('https://cdn.jsdelivr.net/gh/theajack/github-action/', () => {
-    
-// });
-
-console.log('Purge');
--- index.js
+++
@@ -1,3 +0,0 @@
-console.log('Hello world');
-
-module.exports = 'Hello';
