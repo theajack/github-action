4e2e5a05897b4601a1e794ef753a14cc53cb1766
tackchen <tackchen@tencent.com>
2022-04-06 09:39:29 +0800

    feat: test

--- helper/build.js
+++ helper/build.js
@@ -1,12 +1,17 @@
 const gulp = require('gulp');
+const count = require('count-code-line')
 
 console.log(process.argv);
 
 function task (gulpReadme) {
+    count();
     gulp.src(['index.js'])
         .pipe(gulp.dest('npm'));
     gulp.src(['index.js'])
-        .pipe(gulp.dest('npm/package/main'));
+        .pipe(gulp.dest('npm/package/main'))
+        .on('end', () => {
+            count();
+        })
 }
 
 task();
